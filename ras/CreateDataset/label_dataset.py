aa9f485a3ad514434acfdc545eca384871d706c0
Justin <lego.man.123@hotmail.com>
2022-05-19 17:58:56 -0600

    Export location
    - Can name and select the file location to export the csv

--- ras/CreateDataset/label_dataset.py
+++ ras/CreateDataset/label_dataset.py
@@ -750,26 +750,31 @@ class Dataset():
 
         total_distance = utils.get_total_distance(point_pairs, points)/10
         filename = self.get_filename(polygon)
-        
+
+        filename = QFileDialog.getSaveFileName(None, 'Save File', "", "CSV (*.csv)")[0]
         # csv_path = filename.split('.')[0] + '.csv'
         csv_path = participant + '.csv'
+
+        
         data = [participant, filename, points, distance, total_distance]
 
         self.dataframe.loc[0] = data
         try:
-            if os.path.isfile(csv_path):
-                export_csv = self.dataframe.to_csv (csv_path, index = None, header=False, mode='a')
+            if os.path.isfile(filename):
+                export_csv = self.dataframe.to_csv (filename, index = None, header=False, mode='a')
             else:
-                export_csv = self.dataframe.to_csv (csv_path, index = None, header=True, mode='w')
+                export_csv = self.dataframe.to_csv (filename, index = None, header=True, mode='w')
+
+            print("Save Complete")
         except Exception as e:
             print(e)
-            print("Make sure you do not have", csv_path,  "open.")
+            print("Make sure you do not have", filename,  "open.")
 
     def export_all_labels(self, participant, current_folder):
         for polygon in self.labels:
             filename = self.get_filename(polygon)
             load_file = utils.get_global_filename(current_folder, filename)
-
+            
             loaded, image, depth, point_pairs,  points, colours = utils.load_projected(load_file)
             print(loaded, point_pairs)
             self.export_label(polygon, point_pairs, participant)
